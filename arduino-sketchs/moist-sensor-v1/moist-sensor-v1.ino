5fb67ab6958341cdd6655f99316a7fea87c7a7ed
rtmolnar <123@Rap*>
2020-02-23 19:48:34 +0100

    initial sketchs

---
+++ arduino-sketchs/moist-sensor-v1/moist-sensor-v1.ino
@@ -0,0 +1,25 @@
+//Programa: Monitoracao de planta usando Arduino
+//Autor: FILIPEFLOP
+ 
+#define pino_sinal_analogico A0
+int valor_analogico;
+ 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pino_sinal_analogico, INPUT);
+} 
+void loop()
+{
+
+  Serial.println("principio loop");
+  //Le o valor do pino A0 do sensor
+  valor_analogico = analogRead(pino_sinal_analogico);
+ 
+  //Mostra o valor da porta analogica no serial monitor
+  Serial.print("Porta analogica: ");
+  Serial.println(valor_analogico);
+
+  delay(1500);
+}
+ 
---
+++ arduino-sketchs/test-moisture/test-moisture.ino
@@ -0,0 +1,70 @@
+int sense_Pin = 0; // sensor input at Analog pin A0
+int value = 0;
+
+void connectToWiFi(void)
+{
+    client.stop();
+    Serial.println("Conectando-se à rede WiFi...");
+    Serial.println();  
+    delay(1000);
+    WiFi.begin(SSID_REDE, PASSWL);
+ 
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(500);
+        Serial.print(".");
+    }
+ 
+    Serial.println("");
+    Serial.println("WiFi connectado com sucesso!");  
+    Serial.println("IP obtido: ");
+    Serial.println(WiFi.localIP());
+ 
+    delay(1000);
+}
+
+float readMoisture(void)
+{
+    int ValorADC;
+    float UmidadePercentual;
+ 
+     ValorADC = analogRead(0);   //978 -> 3,3V
+     Serial.print("[Leitura ADC] ");
+     Serial.println(ValorADC);
+ 
+     //Quanto maior o numero lido do ADC, menor a umidade.
+     //Sendo assim, calcula-se a porcentagem de umidade por:
+     //      
+     //   Valor lido                 Umidade percentual
+     //      _    264                           _ 100
+     //      |                                |   
+     //      |                                |   
+     //      -   ValorADC                     - UmidadePercentual 
+     //      |                                |   
+     //      |                                |   
+     //     _|_  1024                         _|_ 0
+     //
+      
+     UmidadePercentual = 100 * ((1024-(float)ValorADC) / 1024);
+     Serial.print("[Umidade Percentual] ");
+     Serial.print(UmidadePercentual);
+     Serial.println("%");
+ 
+     return UmidadePercentual;
+}
+
+void setup() {
+   Serial.begin(9600);
+   delay(2000);
+}
+
+void loop() {
+
+   Serial.print("MOISTURE LEVEL : ");
+   value= readMoisture();
+   Serial.println(value);
+
+
+
+   delay(1000);
+}
